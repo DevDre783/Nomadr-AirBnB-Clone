956942a357130a51a0e101edd495f08526874b76
Andres Soca <lumen.development01@gmail.com>
2022-01-06 18:52:42 -0500

    added a validation.js file in utils folder under backend, tested all invalid inputs in the fetch are running with the expected errors -ASoca

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,11 +3,24 @@ const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
 //Log In API behavior
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
@@ -23,7 +36,6 @@ router.post('/', asyncHandler(async (req, res, next) => {
     await setTokenCookie(res, user);
 
     return res.json({
-        message: "success: logged in!",
         user,
     });
 }));
