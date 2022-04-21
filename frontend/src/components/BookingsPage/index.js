3048226ab23ecc5946d37de662f8cf99ffdefa02
Andres Soca <lumen.development01@gmail.com>
2022-04-21 16:49:36 -0400

    fixed the bookings page

--- frontend/src/components/BookingsPage/index.js
+++ frontend/src/components/BookingsPage/index.js
@@ -7,6 +7,8 @@ import { getAllBookings } from "../../store/booking";
 function BookingsPage() {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const allVans = useSelector(state => state.vans.listOfVans);
+
     const userId = sessionUser.id
     console.log("THE ID", userId)
     const allBookings = Object.values(useSelector(state => state.bookings))
@@ -18,10 +20,25 @@ function BookingsPage() {
 
     return (
         <div>
+            <h1 style={{marginTop: "10%", marginLeft: "15%", textDecoration: "underline"}}>My Bookings:</h1>
             {allBookings.map(booking => (
                 <>
                     {userId === booking.userId &&
-                        <h1 key={booking.id} style={{marginTop: "10%", marginLeft: "20%"}}>{booking.startDate} ......TO...... {booking.endDate}</h1>
+                        <>
+                            {/* <h1 key={booking.id} style={{marginTop: "10%", marginLeft: "20%"}}>{booking.startDate} ......TO...... {booking.endDate}</h1> */}
+                            {allVans.map(van => (
+                                <div className="container__picTitle">
+                                    {booking.vanId === van.id &&
+                                        <>
+                                            <h2 style={{marginTop: "5%", textDecoration: "underline"}} key={`${van.id}3`}>{van.title}</h2>
+                                            <Link key={`${van.id}1`} id='vanImgLink' className="vanImage" to={`/vans/${van.id}`}>\
+                                                <img id="vanId" key={`${van.id}2`} src={van?.Images[0]?.url}></img>
+                                            </Link>
+                                        </>
+                                    }
+                                </div>
+                            ))}
+                        </>
                     }
                 </>
             ))}
